5bd1b0bbb0d949a5c1a14ffe00586dd9b10642cd
Lerqiu <mcwk810@gmail.com>
2021-02-02 00:11:26 +0100

    Deleting unselless log

--- js/games/Tic_Tac_Toe.js
+++ js/games/Tic_Tac_Toe.js
@@ -47,12 +47,10 @@ module.exports.socketDo = function (socket, io, room, user, rooms, users) {
                     room.gameData.history.unshift(newStateOfBoard(room.gameData.history[0], arg.y, arg.x, room.gameData.turnNowBy));
 
                     if (didSomeoneWon(room.gameData.history[0])) {
-                        console.log(JSON.stringify(user))
                         io.to(room.id).emit('won', user.getNick());
                         users.addW(user.getNick());
                         rooms.removeRoom(room.id);
                     } else if (isEndOfGame(room.gameData.history[0])) {
-                        console.log(JSON.stringify(user))
                         io.to(room.id).emit('end', '');
                         users.addR(user.getNick());
                         rooms.removeRoom(room.id);
